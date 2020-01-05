b73bbafb1b048509dfc0439ad5d52cd092f1c428
gledsomcb <gledimrockmetal@gmail.com>
2020-01-04 22:12:26 -0300

    finalização

--- index.js
+++ index.js
@@ -1,21 +1,20 @@
-const express = require ('express')
+const express = require('express')
 const app = express()
-const path = require ('path')
+const path = require('path')
 
 const convert = require('./lib/convert')
 
 app.set('view engine', 'ejs')
-app.set ('views', path.join(__dirname, 'views'))
+app.set('views', path.join(__dirname, 'views'))
 app.use(express.static(path.join(__dirname, 'public')))
 
-
-app.get ('/', (req, res) =>{
+app.get('/', (req, res) => {
   res.render('home')
 })
 
-app.get('/cotacao', (req, res) =>{
+app.get('/cotacao', (req, res) => {
   const {cotacao, quantidade} = req.query
-  if(cotacao && quantidade) {
+  if (cotacao && quantidade) {
     const conversao = convert.convert(cotacao, quantidade)
     res.render('cotacao', {
       error: false,
@@ -23,17 +22,17 @@ app.get('/cotacao', (req, res) =>{
       quantidade: convert.toMoney(cotacao),
       conversao: convert.toMoney(conversao)
     })
-  }else{
+  } else {
     res.render('cotacao', {
       error: 'Valores invalidos'
     })
   }
 })
 
-app.listen (3000, err => {
-  if(err) {
+app.listen(3000, err => {
+  if (err) {
     console.log('não foi possivel iniciar')
-  }else{
+  } else {
     console.log('ConvertMyMoney is running')
   }
 })
